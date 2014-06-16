370c8c5ed4130f518488cbccd97bbdd793557647
Eduardo M (bbaaxx) <bbaaxx@gmail.com>
2014-06-16 09:54:55 -0500

    fixed stupid typo

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -3,7 +3,7 @@
 module.exports = {
   port: process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   hostname: process.env.HOSTNAME || 'localhost',
-  process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+  ipaddr: process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
   host: process.env.HOST || 'localhost',
   dbType: 'none yet',
   templateEngine: 'ejs'
