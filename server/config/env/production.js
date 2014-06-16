9fc15398ed3fbb5d8c460473f109d59d7d9d72d9
Eduardo M (bbaaxx) <bbaaxx@gmail.com>
2014-06-16 09:21:23 -0500

    Added openshit stuff to the env config

--- server/config/env/production.js
+++ server/config/env/production.js
@@ -1,8 +1,10 @@
 'use strict';
 
 module.exports = {
-    db: 'Nothing here yet',
-    app: {
-        name: 'Codetitilan, el blog'
-    }
+  port: process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  hostname: process.env.HOSTNAME || 'localhost',
+  process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+  host: process.env.HOST || 'localhost',
+  dbType: 'none yet',
+  templateEngine: 'ejs'
 };
